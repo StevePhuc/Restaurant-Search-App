ba6d55fb441b739a47f848bee69a13d6e443997e
Steve Phuc <stevephuc.dev@gmail.com>
2019-12-26 09:07:12 +0200

    102. Grouping Results

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-const ResultsList = ({ title }) => {
+const ResultsList = ({ title, results }) => {
 
     return (
         <View>
             <Text style={styles.title}> {title}</Text>
+            <Text> {results.length}</Text>
         </View >
     )
 }
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,11 @@ const SearchScreen = () => {
     const [term, setTerm] = useState('')
     const [searchApi, results, errorMessage] = useResults()
 
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price
+        })
+    }
 
     return (
         <View>
@@ -18,9 +23,9 @@ const SearchScreen = () => {
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length}</Text>
-            <ResultsList title="Const Effective" />
-            <ResultsList title="Bit Pricier" />
-            <ResultsList title="Big Spender" />
+            <ResultsList title="Const Effective" results={filterResultsByPrice('$')} />
+            <ResultsList title="Bit Pricier" results={filterResultsByPrice('$$')} />
+            <ResultsList title="Big Spender" results={filterResultsByPrice('$$$')} />
         </View >
     )
 }
