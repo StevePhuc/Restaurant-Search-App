a438854c8c9b9a4d4fd9465841a85ddb23c01899
Steve Phuc <stevephuc.dev@gmail.com>
2019-12-26 18:37:46 +0200

    111. Reminder on Navigation

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+
+const ResultsShowScreen = () => {
+    return (
+        <>
+            <Text>ResultsShowScreen</Text>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ResultsShowScreen
